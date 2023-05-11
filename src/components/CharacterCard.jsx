0113a56a8b8789efe8bdbece105d68433be2f0bf
vemoarrozypollo <veraomar50@gmail.com>
2023-05-11 14:03:29 -0500

    add img card

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
+import { Card } from 'react-bootstrap';
 
 const CharacterCard = ({ url }) => {
   const [character, setCharacter] = useState({})
@@ -13,25 +14,48 @@ const CharacterCard = ({ url }) => {
   let hectograms = character.weight
   let weightkg = (hectograms * (1 / 10)).toFixed(2)
   let types = character.types
-
+  let abilities = character.abilities
+  let stats = character.stats
   return (
-    <div>
-      <h2>{character.name}</h2>
-      <img src={character.sprites?.other.home.front_default ?
+    <Card className='cardback my-5 border border-dark rounded-4' style={{  width: '420px' , height:'590px' }}>
+      <Card.Title>
+        {character.name}
+        </Card.Title>
+      <Card.Img className='imgpoke'  style={{  width: '350px' , height:'240px' }} src={character.sprites?.other.home.front_default ?
         character.sprites?.other.home.front_default :
         character.sprites?.front_default} alt='' />
-      <h4>{heightmeters}m</h4>
-      <h4>{weightkg}kg</h4>
-      {types &&
-        <div> <h3><b>Types:</b></h3>
-          {types.map(type => (
-            <h4 key={type.type.name}>
-              {type.type.name}
-            </h4>
+        <Card.Body className='mx-2 my-1'>
+        {abilities &&
+        <div> <b>Abilitys:</b>
+          {abilities.map(abilitys => (
+            <div key={abilitys.ability.name}>
+              {abilitys.ability.name}
+            </div>
           ))}
         </div>
       }
-    </div>
+      {heightmeters}m
+      {weightkg}kg
+      {types &&
+        <div> <b>Types:</b>
+          {types.map(type => (
+            <div key={type.type.name}>
+              {type.type.name}
+            </div>
+          ))}
+        </div>
+      }
+      {stats &&
+        <div> <b>Stats:</b>
+          {stats.map(stas => (
+            <div key={stas.stat.name}>
+              {stas.stat.name}:{stas.base_stat}
+            </div>
+          ))}
+        </div>
+      }
+      </Card.Body>
+    </Card>
   );
 };
 
