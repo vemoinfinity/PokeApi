ab0ffc8eb1d70209a0edbe7b7f313b46881056f5
vemoarrozypollo <veraomar50@gmail.com>
2023-06-08 17:38:29 -0500

    optimation hooks

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -5,23 +5,33 @@ import { changeName } from '../store/slices/userName.slice';
 import axios from 'axios';
 
 const useLogin = () => {
-
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [character, setCharacter] = useState("")
-  const getRandomInt = Math.floor(Math.random() * 1279)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [character, setCharacter] = useState("");
+  const getRandomInt = Math.floor(Math.random() * 1279);
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${getRandomInt}&limit=1`)
-      .then(res => setCharacter(res.data.results[0]?.url))
-  }, [])
+    const getRandomCharacter = async () => {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${getRandomInt}&limit=1`);
+        const characterUrl = response.data.results[0]?.url;
+        setCharacter(characterUrl);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getRandomCharacter();
+  }, []);
+
   const submit = (data) => {
-    dispatch(changeName(data.name))
-    navigate("/characters")
-  }
+    dispatch(changeName(data.name));
+    navigate("/characters");
+  };
+
   return {
     character,
     submit
-  }
+  };
 };
 
 export default useLogin;
--- src/hooks/usePokeApi.jsx
+++ src/hooks/usePokeApi.jsx
@@ -3,62 +3,63 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const usePokeApi = (url) => {
-  const pokemon = useSelector(state => (state.pokeName))
+  const pokemon = useSelector(state => state.pokeName);
 
-  const [pokeunits, setPokeUnits] = useState([])
-  const [characters, setCharacters] = useState([])
-  const [searchTerm, setSearchTerm] = useState([])
-  const [locationList, setLocationList] = useState([])
-  const [pagnext, setPagNext] = useState("")
-  const [Pokeurl, setUrl] = useState([]);
+  const [pokeunits, setPokeUnits] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+  const [locationList, setLocationList] = useState([]);
+  const [pagnext, setPagNext] = useState("");
+  const [pagpre, setPagPre] = useState("");
+  const [countup, setCountUp] = useState(42);
+  const [countdo, setCountDo] = useState(0);
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=42`)
-      .then(res => setCharacters(res.data.results))
+      .then(res => {
+        setCharacters(res.data.results);
+        setPagNext(res.data.next);
+      });
     axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1281`)
-      .then(res => setSearchTerm(res.data.results))
-    axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=42')
-      .then(res => setPagNext(res.data.next))
+      .then(res => setSearchTerm(res.data.results));
     axios.get(`https://pokeapi.co/api/v2/location/`)
-      .then(res => setLocationList(res.data.results))
+      .then(res => setLocationList(res.data.results));
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      .then(res => setPokeUnits(res.data))
-  }, [])
-  const urlpokeunits = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-  const [pagpre, setPagPre] = useState("")
-  const [countup, setCountUp] = useState(42)
-  const [countdo, setCountDo] = useState(0)
+      .then(res => setPokeUnits(res.data));
+  }, [pokemon]);
 
   const pagenext = () => {
     if (countup < 1280) {
       axios.get(pagnext)
-        .then(res => setCharacters(res.data.results))
-      axios.get(pagnext)
-        .then(res => setPagPre(res.data.previous))
-      axios.get(pagnext)
-        .then(res => setPagNext(res.data.next))
-      setCountUp(countup + 42)
-      setCountDo((countdo + 42))
+        .then(res => {
+          setCharacters(res.data.results);
+          setPagPre(res.data.previous);
+          setPagNext(res.data.next);
+        });
+      setCountUp(countup + 42);
+      setCountDo(countdo + 42);
+    } else {
+      alert('Error: No hay más páginas');
     }
-    else {
-      alert('erros ya no hay mas paginas')
-    }
-  }
+  };
+
   const pageprev = () => {
     if (countdo > 0) {
       axios.get(pagpre)
-        .then(res => setCharacters(res.data.results))
-      axios.get(pagpre)
-        .then(res => setPagNext(res.data.next))
-      axios.get(pagpre)
-        .then(res => setPagPre(res.data.previous))
-      setCountUp(countup - 42)
-      setCountDo((countdo - 42))
+        .then(res => {
+          setCharacters(res.data.results);
+          setPagNext(res.data.next);
+          setPagPre(res.data.previous);
+        });
+      setCountUp(countup - 42);
+      setCountDo(countdo - 42);
+    } else {
+      alert('Error: No hay más páginas');
     }
-    else {
-      alert('erros ya no hay mas paginas')
-    }
-  }
+  };
+
+  const urlpokeunits = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
   return {
     characters,
     locationList,
